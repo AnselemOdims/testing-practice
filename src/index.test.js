bd46aec4f1547ccce3b0957d42c6a19b26557297
Anselem <anselemodims@gmail.com>
2021-12-07 14:13:46 +0100

    Put all the stringLength function tests in a describe function

--- src/index.test.js
+++ src/index.test.js
@@ -1,58 +1,54 @@
 import {stringLength, reverseString} from './index.js';
 
-//run test if valid
-test('function returns a string length if string length is between 1 and 10', () => {
+describe('stringLength function', ()=> {
   const myString = 'hello';
   const strLength = myString.length;
-  expect(stringLength(`${myString}`)).toBe(strLength);
-});
 
-test('function returns a number if string length is between 1 and 10', () => {
-  const myString = 'hello';
-  const strLength = myString.length;
-  expect(typeof(stringLength(`${myString}`)) === 'number').toBeTruthy();
-});
+  describe('Valid tests', ()=> {
+    test('returns a string length if string length is between 1 and 10', () => {
+      expect(stringLength(`${myString}`)).toBe(strLength);
+    });
+    
+    test('returns a number if string length is between 1 and 10', () => {
+      expect(typeof(stringLength(`${myString}`)) === 'number').toBeTruthy();
+    });
+    
+    test('returns a number if string length is between 1 and 10', () => {
+      expect(typeof(stringLength(`${myString}`)) === 'number').not.toBeFalsy();
+    });
+    
+    test('returns a string length if string length is between 1 and 10', () => {
+      expect(stringLength(`${myString}`)).not.toBeNull();
+    });
+    
+    test('returns a string length if string length is between 1 and 10', () => {
+      expect(stringLength(`${myString}`)).toEqual(5);
+    });
+  })
+  
+  
+  describe('Not valid tests', ()=> {
+    const myString = 'Hello World';
+    const error = 'Error: Not length not btw 1 and 10';
 
-test('function returns a number if string length is between 1 and 10', () => {
-  const myString = 'hello';
-  const strLength = myString.length;
-  expect(typeof(stringLength(`${myString}`)) === 'number').not.toBeFalsy();
-});
-
-test('function returns a string length if string length is between 1 and 10', () => {
-  const myString = 'hello';
-  const strLength = myString.length;
-  expect(stringLength(`${myString}`)).not.toBeNull();
-});
-
-test('function returns a string length if string length is between 1 and 10', () => {
-  const myString = 'hello';
-  const strLength = myString.length;
-  expect(stringLength(`${myString}`)).toEqual(5);
-});
-
-//Invalid tests
-test('function returns an error if string length is not between 1 and 10', () => {
-  const myString = 'Hello World';
-  const error = 'Error: Not length not btw 1 and 10';
-  expect(stringLength(`${myString}`)).toBe(error);
-});
-
-test('function returns an error if input is an empty string', () => {
-  const myString = '';
-  const error = 'Error: Not length not btw 1 and 10';
-  expect(stringLength(`${myString}`)).toBe(error);
-});
-
-test('function returns an error if input is an empty string', () => {
-  const myString = 'Hello World';
-  expect(typeof(stringLength(`${myString}`)) === 'string').toBeTruthy();
-});
-
-test('function returns an error if input is an empty string', () => {
-  const myString = 'Hello World';
-  expect(stringLength(`${myString}`)).not.toBeNull();
-});
+    test('returns an error if string length is not between 1 and 10', () => {
+      expect(stringLength(`${myString}`)).toBe(error);
+    });
+    
+    test('returns an error if input is an empty string', () => {
+      const myString = '';
+      expect(stringLength(`${myString}`)).toBe(error);
+    });
+    
+    test('returns an error if input is an empty string', () => {
+      expect(typeof(stringLength(`${myString}`)) === 'string').toBeTruthy();
+    });
+    
+    test('returns an error if input is an empty string', () => {
+      expect(stringLength(`${myString}`)).not.toBeNull();
+    });
+  })
+})
 
 
 // REVERSE A STRING FUNCTION
