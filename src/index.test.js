5115b65d78a3320efa914513a5fceeecdc518ce5
Anselem <anselemodims@gmail.com>
2021-12-07 13:32:50 +0100

    Added two more tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,21 @@
 import stringLength from './index.js';
 
+//run test if valid
 test('function returns a string length if string length is between 1 and 10', () => {
   const myString = 'hello';
   const strLength = myString.length;
   expect(stringLength(`${myString}`)).toBe(strLength);
 });
+
+test('function returns a number if string length is between 1 and 10', () => {
+  const myString = 'hello';
+  const strLength = myString.length;
+  expect(typeof(stringLength(`${myString}`)) === 'number').toBeTruthy();
+});
+
+test('function returns a number if string length is between 1 and 10', () => {
+  const myString = 'hello';
+  const strLength = myString.length;
+  expect(typeof(stringLength(`${myString}`)) === 'number').not.toBeFalsy();
+});
+
